b5b7c72a0cc93a396e96bac382ad2ddb645fbc61
Md Saber Hossain <mhmcb@missouri.edu>
2025-06-04 19:00:41 -0500

    configure host + node

---
+++ install/docker/init.sql
@@ -0,0 +1,8 @@
+-- Central API schema
+CREATE DATABASE arachne_portal;
+-- Node API schema
+CREATE DATABASE arachne_datanode;
+
+-- Optional: users, schemas, extensions
+-- \connect arachne_portal;
+-- CREATE USER ohdsi WITH PASSWORD 'ohdsi';
